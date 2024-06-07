5a8d36d3edaa8ecd980bd553fbf22f2ab2b28f3b
Rahil <prajapatirahil13@gmail.com>
2024-06-07 18:21:45 +0530

    setup Configured and postman

--- src/app.js
+++ src/app.js
@@ -15,4 +15,15 @@ app.use(express.urlencoded({extended: true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+// Routes import
+import userRouter from "./routes/user.routes.js"
+
+// Routes Declaration
+app.use("/api/v1/users",userRouter)
+
+
+
+
+
 export {app}
